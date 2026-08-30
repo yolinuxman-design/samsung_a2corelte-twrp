0afb1947b7d3c6e47f5bbd4b8c6a588a716a4858
yolinuxman-design <yolinuxman@gmail.com>
2026-08-30 23:23:11 +0545

    Update omni_a2corelte.mk

--- omni_a2corelte.mk
+++ omni_a2corelte.mk
@@ -7,7 +7,7 @@ $(call inherit-product, $(SRC_TARGET_DIR)/product/aosp_base_telephony.mk)
 $(call inherit-product, vendor/omni/config/common.mk)
 
 PRODUCT_COPY_FILES += \
-     $(LOCAL_PATH)/prebuilt/Image:kernel \
+     $(LOCAL_PATH)/prebuilt/kernel:kernel \
      $(LOCAL_PATH)/prebuilt/dt.img:dt.img \
      $(LOCAL_PATH)/prebuilt/dt.img:boot.img
 
