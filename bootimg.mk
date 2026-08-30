f21a3218b7891af11046958a7e2c945452c3fcea
yolinuxman-design <yolinuxman@gmail.com>
2026-08-31 00:50:29 +0545

    Update bootimg.mk

--- bootimg.mk
+++ bootimg.mk
@@ -2,6 +2,12 @@ LOCAL_PATH := $(call my-dir)
 
 FLASH_IMAGE_TARGET ?= $(PRODUCT_OUT)/recovery.tar
 
+# Boot image rule (required by Pie build system as dependency)
+$(INSTALLED_BOOTIMAGE_TARGET): $(MKBOOTIMG) $(INTERNAL_BOOTIMAGE_FILES) $(BOARD_CUSTOM_BOOTIMG_MK)
+	@echo -e ${CL_GRN}"----- Making boot image ------"${CL_RST}
+	$(hide) $(MKBOOTIMG) $(INTERNAL_BOOTIMAGE_ARGS) $(BOARD_MKBOOTIMG_ARGS) --output $@
+	@echo -e ${CL_CYN}"Made boot image: $@"${CL_RST}
+
 $(INSTALLED_RECOVERYIMAGE_TARGET): $(MKBOOTIMG) $(INSTALLED_DTIMAGE_TARGET) $(recovery_kernel) $(recovery_ramdisk)
 	@echo -e ${CL_GRN}"----- Making recovery image ------"${CL_RST}
 	$(hide) $(MKBOOTIMG) $(INTERNAL_RECOVERYIMAGE_ARGS) $(BOARD_MKBOOTIMG_ARGS) --output $@ --ramdisk $(recovery_ramdisk)
