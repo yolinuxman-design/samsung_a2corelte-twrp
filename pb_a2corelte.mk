788f66c80a708d2a9cfabe4a90096ae746bc195f
yolinuxman-design <yolinuxman@gmail.com>
2026-09-06 11:44:10 +0545

    a2core: init pb

--- pb_a2corelte.mk
+++ pb_a2corelte.mk
@@ -4,7 +4,7 @@ PRODUCT_RELEASE_NAME := a2corelte
 $(call inherit-product, build/target/product/embedded.mk)
 
 # Inherit from our custom product configuration
-$(call inherit-product, vendor/omni/config/common.mk)
+$(call inherit-product, vendor/pb/config/common.mk)
 
 PRODUCT_PACKAGES += \
 	charger_res_images \
@@ -12,7 +12,7 @@ PRODUCT_PACKAGES += \
 
 ## Device identifier. This must come after all inclusions
 PRODUCT_DEVICE := a2corelte
-PRODUCT_NAME := omni_a2corelte
+PRODUCT_NAME := pb_a2corelte
 PRODUCT_MODEL := Galaxy A2 Core (2019)
 PRODUCT_BRAND := samsung
 PRODUCT_MANUFACTURER := samsung
