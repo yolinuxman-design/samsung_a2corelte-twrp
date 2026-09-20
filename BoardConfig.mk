4344b6393a9c4cd928a4ad31a2e572b9f8223c05
yolinuxman-design <yolinuxman@gmail.com>
2026-09-20 23:56:38 +0545

    Fixes for mtp and added support for sd card

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -114,3 +114,12 @@ TARGET_USES_LOGD := true
 TW_MAINTAINER := Rabi Rauniyar
 TW_DEVICE_VERSION := 2.0
 RECOVERY_VARIANT := TWRP
+
+
+# Storage & MTP configuration flags
+TW_INCLUDE_MTP := true
+TW_INTERNAL_STORAGE_PATH := "/sdcard"
+TW_INTERNAL_STORAGE_MOUNT_POINT := "sdcard"
+TW_EXTERNAL_STORAGE_PATH := "/external_sd"
+TW_EXTERNAL_STORAGE_MOUNT_POINT := "external_sd"
+
